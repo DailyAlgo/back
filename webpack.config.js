45427ffe66b6ced864e692f864e51685cd5cd234
chen.kim <chen.kim@kakaocorp.com>
2022-12-18 19:36:40 +0900

    [SETTING] dev webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,61 @@
 const path = require('path')
+const NodemonPlugin = require('nodemon-webpack-plugin')
+const nodeExternals = require('webpack-node-externals')
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
+
 const isDev = process.env.NODE_ENV !== 'production'
+
+const fileLoader = {
+  loader: 'file-loader',
+  options: {
+    name: '[path][name].[contenthash:16].[ext]',
+    context: path.resolve(__dirname, 'src/assets'),
+    publicPath: '/static',
+    outputPath: 'static',
+  },
+}
 
 const serverConfig = {
   entry: './index.ts',
   context: path.resolve(__dirname, 'src'),
-  mode: isDev? 'development' : 'production',
-  // devtools: 'source-map',
+  mode: isDev ? 'development' : 'production',
+  devtool: 'source-map',
   target: 'node', // node를 대상으로 설정
+  externals: [nodeExternals()],
   module: {
-      rules: [
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: [/node_modules/],
+        use: [
           {
-              test: /\.tsx?$/,
-              exclude: [/node_modules/],
-              use: [
-                  {
-                      loader: 'ts-loader',
-                  },
-              ],
+            loader: 'ts-loader',
           },
-      ]
+        ],
+      },
+    ],
   },
   resolve: {
-      extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
   },
   output: {
-      filename: 'index.js',
-      path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  node: {
+    __dirname: false,
+  },
+  plugins: [
+    new NodemonPlugin({
+      watch: path.resolve('./dist'),
+      script: './dist/index.js',
+    }),
+    ...(isDev ? [] : []),
+  ],
+  optimization: {
+    minimize: false,
   },
 }
-
-
 
 module.exports = [serverConfig]
