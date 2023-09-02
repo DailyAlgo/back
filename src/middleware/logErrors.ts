c2e827ccc7f22b2e659a0a857d42b763d86dce76
dvlprjw <dvlprjw@gmail.com>
2023-09-02 21:24:56 +0900

    fix: error response, send -> json

--- src/middleware/logErrors.ts
+++ src/middleware/logErrors.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 const middleware = (err: Error,  req: Request, res: Response, next: NextFunction) => {
   console.log("** ERROR LOGGING **")
-  console.log(req.url)
+  console.log(req.originalUrl)
   console.error(err.stack)
   next(err)
 }
