2603bd17d406a2c3f49a9a8430f117b5e515aea4
chen.kim <chen.kim@kakaocorp.com>
2023-09-02 20:22:54 +0900

    fix: error handler render -> send

--- src/middleware/errorHandler.ts
+++ src/middleware/errorHandler.ts
@@ -1,8 +1,12 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from 'express'
 
-const middleware = (err: Error,  req: Request, res: Response, next: NextFunction) => {
-  res.status(500)
-  res.render('error', { error: err })
+const middleware = (
+  err: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  res.status(500).send({ message: err })
 }
 
 export default middleware
