95db14132f180ced105e064f15df9c20f2193f33
dvlprjw <dvlprjw@gmail.com>
2023-09-02 21:30:00 +0900

    fix: NOT FOUND 에러 404 처리

--- src/middleware/clientErrorHandler.ts
+++ src/middleware/clientErrorHandler.ts
@@ -1,7 +1,11 @@
+import { HttpStatusCode } from "axios";
 import { NextFunction, Request, Response } from "express";
 
 const middleware = (err: Error,  req: Request, res: Response, next: NextFunction) => {
-  if (req.xhr) {
+  if (err instanceof Error && err.message === 'NOT_FOUND') {
+    res.status(HttpStatusCode.NotFound)
+  }
+  else if (req.xhr) {
     res.status(500).send({ error: 'This Request occurs an Error' })
   } else {
     next(err)
