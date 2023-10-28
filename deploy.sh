0ae9d55abe693287e68f0e34b6aad38dd79399e6
dvlprjw <dvlprjw@gmail.com>
2023-10-29 02:53:48 +0900

    fix: 원복

--- deploy.sh
+++ deploy.sh
@@ -11,6 +11,6 @@ sudo git pull origin main
 echo redis dump 파일 권한 부여 : 권한 설정
 sudo chmod 777 ./redis/data/dump.rdb
 echo 도커 컴포즈 실행
-docker-compose up -d
+docker-compose up -d --force-recreate
 echo 도커 컴포즈 로그 출력
 docker-compose logs
