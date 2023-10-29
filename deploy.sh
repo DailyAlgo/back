df4226fb81f7985add97d297c398e247346cfdc4
dvlprjw <dvlprjw@gmail.com>
2023-10-29 23:24:07 +0900

    fix: delpoy 파일 변경

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,8 @@ echo 깃 풀
 sudo git pull origin main
 echo redis dump 파일 권한 부여 : 권한 설정
 sudo chmod 777 ./redis/data/dump.rdb
+echo 도커 이미지 빌드
+sudo docker build -t dvlprjw/dailyalgo_be .
 echo 도커 컴포즈 실행
 docker-compose up -d --force-recreate
 echo 도커 컴포즈 로그 출력
