b93d39c92e205416be32e5c81f1d09265441e75f
dvlprjw <dvlprjw@gmail.com>
2023-11-26 17:41:10 +0900

    fix: signup check id -> email

--- src/middleware/check_certification_num.ts
+++ src/middleware/check_certification_num.ts
@@ -3,8 +3,12 @@ import redis from '../service/redis'
 
 const middleware = async (req: Request, res: Response, next: NextFunction) => {
   try {
+    if (!req.body.num)
+      return res.status(400).json({ message: 'num이 Null 값입니다.' })
+    if (!req.body.email)
+      return res.status(400).json({ message: 'email이 Null 값입니다.' })
     const num: string = req.body.num
-    const certificationNum: string = await redis.get(req.body.id, true)
+    const certificationNum: string = await redis.get(req.body.email, true)
     if (num !== certificationNum) throw new Error('num is not qualified')
     next()
   } catch (error) {
