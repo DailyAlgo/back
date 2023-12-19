e412a2899e33162b0ba6dc4f2a4b962899cedf2e
dvlprjw <dvlprjw@gmail.com>
2023-12-19 20:16:01 +0900

    fix: 비밀번호 체크 로직 수정

--- src/middleware/check_password.ts
+++ src/middleware/check_password.ts
@@ -3,7 +3,9 @@ import passwordService from '../service/password'
 
 const middleware = async (req: Request, _: Response, next: NextFunction) => {
   try {
-    await passwordService.compare(req.body.id, req.body.password)
+    const id = req.credentials?.user.id
+    const password = req.body.password
+    await passwordService.compare(id, password)
     next()
   } catch (error) {
     console.log(`!! 비밀번호 틀림`)
