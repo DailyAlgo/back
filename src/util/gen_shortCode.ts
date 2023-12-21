7c460fce99bfe67f4afb476f423f2a92829504cd
dvlprjw <dvlprjw@gmail.com>
2023-12-22 00:47:08 +0900

    fix: hash 생성 로직변경

--- src/util/gen_shortCode.ts
+++ src/util/gen_shortCode.ts
@@ -3,11 +3,11 @@ import { findIfExistByCode } from '../controller/organization'
 
 const getCode_6_digits = async (value: string, salt: string): Promise<string> => {
   const hash = await bcrypt.hash(value, salt)
-  return hash.slice(66,72)
+  return hash.slice(54,60)
 }
 
 export const getOrganizationCode = async (name: string): Promise<string> => {
-  const salt = await bcrypt.genSalt(3)
+  const salt = await bcrypt.genSalt(10)
   let code = await getCode_6_digits(name, salt)
   while ((await findIfExistByCode(code)) ) {
     code = await getCode_6_digits(code, salt)
