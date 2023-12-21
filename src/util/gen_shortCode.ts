0836d2c9ae9320f38fbc79938141d8f6c6236ba1
dvlprjw <dvlprjw@gmail.com>
2023-12-22 00:35:12 +0900

    fix: hash 자리 수 수정

--- src/util/gen_shortCode.ts
+++ src/util/gen_shortCode.ts
@@ -3,11 +3,11 @@ import { findIdByCode } from '../controller/organization'
 
 const getCode_6_digits = async (value: string, salt: string): Promise<string> => {
   const hash = await bcrypt.hash(value, salt)
-  return hash.slice(0,6)
+  return hash.slice(66,72)
 }
 
 export const getOrganizationCode = async (name: string): Promise<string> => {
-  const salt = await bcrypt.genSalt(1)
+  const salt = await bcrypt.genSalt(3)
   let code = await getCode_6_digits(name, salt)
   while (await findIdByCode(code) < 0) {
     code = await getCode_6_digits(code, salt)
