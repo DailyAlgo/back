5837b592106b6bdf87ed4aebb08e4eb4d26852eb
dvlprjw <dvlprjw@gmail.com>
2024-01-17 21:20:00 +0900

    fix: unreadonly 수정

--- src/controller/notification.ts
+++ src/controller/notification.ts
@@ -76,8 +76,7 @@ export const findNotificationList = async (
   try {
     if (!req.credentials?.user)
       return res.status(400).json({ message: 'User Info is missing' })
-    const unreadOnly = req.query['unreadOnly'] ? Boolean(req.query['unreadOnly']) : false
-    console.log(unreadOnly)
+    const unreadOnly = req.query['unreadOnly'] ? req.query['unreadOnly']==='true' : false
     const offset = req.query['offset'] ? Number(req.query['offset']) : 0
     res.status(200).json(await notificationService.finds(req.credentials.user.id, unreadOnly, offset))
   } catch (error) {
